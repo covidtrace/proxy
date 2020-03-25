0dc9ec6c06b0c7af6f08048e8a35c373364b6c29
Josh Gummersall <josh@standardlabs.dev>
2020-03-25 13:40:49 -0700

    Bump RPH limit

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ gcloud functions deploy Proxy \
   --trigger-http \
   --allow-unauthenticated \
   --vpc-connector default \
-  --set-env-vars BACKEND_SERVICE=https://notary-k3cimrd2pq-uc.a.run.app,REDIS_HOST=10.72.126.123:6379,REQUESTS_PER_HOUR=20
+  --set-env-vars BACKEND_SERVICE=https://notary-k3cimrd2pq-uc.a.run.app,REDIS_HOST=10.72.126.123:6379,REQUESTS_PER_HOUR=100
