72dfa4b8f57a4b1f6102ce16a1208dcf6deb4ad3
Josh Gummersall <josh@standardlabs.dev>
2020-03-30 10:03:12 -0700

    Verify `jwt` on notary requests

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 for function in Notary Operator; do
   gcloud functions deploy "${function}" \
@@ -9,5 +9,5 @@ for function in Notary Operator; do
     --trigger-http \
     --allow-unauthenticated \
     --vpc-connector default \
-    --env-vars-file env.yaml
+    --set-env-vars JWT_SIGNING_KEY=${JWT_SIGNING_KEY},NOTARY_BACKEND="https://notary-k3cimrd2pq-uc.a.run.app",NOTARY_QPH="50",OPERATOR_BACKEND="https://operator-k3cimrd2pq-uc.a.run.app",OPERATOR_QPH="5",REDIS_HOST="10.72.126.123:6379"
 done
--- proxy.go
+++ proxy.go
@@ -8,38 +8,14 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-redis/redis/v7"
 	redisrate "github.com/go-redis/redis_rate/v8"
 )
 
-func getJwtToken(audience string) (string, error) {
-	req, err := http.NewRequest(
-		"GET",
-		fmt.Sprintf(
-			"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=%s",
-			url.QueryEscape(audience),
-		),
-		nil,
-	)
-	if err != nil {
-		return "", err
-	}
-
-	req.Header.Set("Metadata-Flavor", "Google")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
+var jwtSigningKey []byte
 
 var notaryBackend string
 var notaryQph int
@@ -69,6 +45,34 @@ func getEnvInt(key string) int {
 	return val
 }
 
+func getGoogleJWT(audience string) (string, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=%s",
+			url.QueryEscape(audience),
+		),
+		nil,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Metadata-Flavor", "Google")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
 func buildDirector(b string) func(r *http.Request) {
 	bu, err := url.Parse(b)
 	if err != nil {
@@ -80,13 +84,15 @@ func buildDirector(b string) func(r *http.Request) {
 		r.URL.Host = bu.Host
 		r.Host = bu.Host
 
-		if jwt, err := getJwtToken(b); err == nil {
+		if jwt, err := getGoogleJWT(b); err == nil {
 			r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", jwt))
 		}
 	}
 }
 
 func init() {
+	jwtSigningKey = []byte(getEnv("JWT_SIGNING_KEY"))
+
 	notaryBackend = getEnv("NOTARY_BACKEND")
 	notaryQph = getEnvInt("NOTARY_QPH")
 	notaryProxy = httputil.ReverseProxy{Director: buildDirector(notaryBackend)}
@@ -123,6 +129,45 @@ func Notary(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("x-ratelimit-allowed", "false")
 		// http.Error(w, "Rate limit exceeded", 429)
 		// return
+	}
+
+	authorization := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(authorization) != 2 {
+		http.Error(w, "Missing `Authorization` header", 401)
+		return
+	}
+
+	if !strings.EqualFold(authorization[0], "bearer") {
+		http.Error(w, "Only `Bearer` authorization type supported", 401)
+		return
+	}
+
+	token, err := jwt.Parse(authorization[1], func(t *jwt.Token) (interface{}, error) {
+		if t == nil {
+			return nil, fmt.Errorf("Token is nil")
+		}
+
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return jwtSigningKey, nil
+	})
+
+	if err != nil || token == nil || !token.Valid {
+		http.Error(w, "Invalid `Authorization` header", 401)
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		http.Error(w, "Invalid jwt token", 401)
+		return
+	}
+
+	if iss, ok := claims["iss"]; !ok || iss != "covidtrace/operator" {
+		http.Error(w, "Invalid `iss` claim", 401)
+		return
 	}
 
 	notaryProxy.ServeHTTP(w, r)
