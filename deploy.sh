1c7f70020928ce34a78b3e4923045a6ff3649f2e
Josh Gummersall <josh@standardlabs.dev>
2020-04-10 11:28:56 -0700

    Remove env vars as they are expressed in terraform

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,5 @@ for function in Notary Operator Emails; do
     --runtime go113 \
     --memory 128 \
     --trigger-http \
-    --allow-unauthenticated \
-    --vpc-connector "${VPC_CONNECTOR}" \
-    --set-env-vars JWT_SIGNING_KEY="${JWT_SIGNING_KEY}",NOTARY_BACKEND="${NOTARY_BACKEND}",NOTARY_QPH="${NOTARY_QPH}",OPERATOR_BACKEND="${OPERATOR_BACKEND}",OPERATOR_QPH="${OPERATOR_QPH}",EMAILS_BACKEND="${EMAILS_BACKEND}",EMAILS_QPH="${EMAILS_QPH}",REDIS_HOST="${REDIS_HOST}"
+    --allow-unauthenticated
 done
